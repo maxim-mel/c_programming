049d3322a0ff8cae55373b35b61d8ae83a9ef52d
maxim-mel <62463135+maxim-mel@users.noreply.github.com>
2020-03-26 13:21:21 +0300

    Task 1

---
+++ practice_march26/task1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define R 5
+#define C 6
+
+int main()
+{
+    int a[R][C];
+    int i, j;
+
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
+            a[i][j] = rand()%100;
+        }
+    }
+
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (i = 0; i < C; i++) {
+        int min = 1000;
+        for (j = 0; j < R; j++) {
+            if (a[j][i] < min) {
+                min = a[j][i];
+            }
+        }
+        printf("The minimum value in column %d is %d.", i+1, min);
+        printf("\n");
+    }
+
+
+    return 0;
+}
